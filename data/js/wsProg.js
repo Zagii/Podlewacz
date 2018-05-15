a3703af644797939780a1f92fb3708aa532a4b2f
kzg <krzysztof.zagawa@external.t-mobile.pl>
2018-05-15 14:58:43 +0200

    konfiguracja html

--- data/js/wsProg.js
+++ data/js/wsProg.js
@@ -1,14 +1,16 @@
-//var wsSerw="192.168.1.144";
-var progN=0;
+//let wsSerw="192.168.1.144";
+let progN=0;
 document.addEventListener("DOMContentLoaded", function(event) {
     debug=document.getElementById('deb');
     console.log("document On load");
-    var fileName = location.pathname.split("/").slice(-1);
+    let fileName = location.pathname.split("/").slice(-1);
     //alert (fileName);
+    delSekcje();
     for(i=1;i<7;i++)
     {
         progN=addProg(i,i*10000,i*100,i,i,i%2)
         console.log("Dodaje: "+i);
+        addSekcja(i,"Sekcja "+i);
     }
     
     document.getElementById('godz').addEventListener('change', function() {
@@ -53,12 +55,12 @@ function addProg(i,dd,ile_s,coIle_d,sek,akt) {
     console.log("add: "+i);
     
     dt=new Date(dd);
-    var w = document.createElement("div"); 
+    let w = document.createElement("div"); 
     w.className="w3-row w3-center w3-padding-16 w3-section";
     if(akt)w.classList.add("w3-light-grey");
     else w.classList.add("w3-dark-grey");
     w.id="ProgW"+i;
-	var k= document.createElement("div");
+	let k= document.createElement("div");
     k.className="w3-third w3-section";
     c1a=document.createElement("div");
     c1a.className="w3-half w3-section";
@@ -106,7 +108,7 @@ function addProg(i,dd,ile_s,coIle_d,sek,akt) {
     
    }
    c3b1.addEventListener('click',function(){
-       var el=document.getElementById("Prog"+i);
+       let el=document.getElementById("Prog"+i);
       // alert("Prog"+i+" stan="+el.getAttribute('data-stan'));
       if(el.getAttribute('data-stan')==1)
       {
@@ -136,7 +138,7 @@ function addProg(i,dd,ile_s,coIle_d,sek,akt) {
                 " <small id=\"bs"+i+"\">OFF</small>"+           
                 " <p class=\"w3-wide\" >Sekcja "+i+"\</p>"+
                 " <i class=\"fa fa-shower fa-5x\" style=\"color:darkred\" id=\"bi"+i+"\"></i></button>";     */
-    var foo = document.getElementById("prog");
+    let foo = document.getElementById("prog");
     foo.appendChild(w);
    return i;
   }
@@ -146,4 +148,50 @@ function addProg(i,dd,ile_s,coIle_d,sek,akt) {
         alert("usuwam");
     }
 }
-  
+function delSekcje()
+{
+    let f=document.getElementById("addProgForm").elements["sekcja"];
+   while(f.length>0)
+    {
+       f.remove(f.length-1);
+    }
+}
+function addSekcja(v,n)
+{
+    let o = document.createElement("option");
+    o.text = n;
+    o.value=v;
+    document.getElementById("addProgForm").elements["sekcja"].add(o);
+    addSekList(v,n);
+}
+function addSekList(v,n)
+{
+    let f=document.getElementById("sekList");
+    let w = document.createElement("div"); 
+    w.className="w3-row w3-center w3-padding-8 w3-section";
+    if(v%2)w.classList.add("w3-light-grey");
+    else w.classList.add("w3-dark-grey");
+    w.id="Sek"+i;
+	//let k= document.createElement("div");
+    //k.className="w3-third w3-section";
+    c1a=document.createElement("div");
+    c1a.className="w3-col m2 w3-section";
+    c1a.innerHTML="<h4>"+v+"</h4>";
+    //k.appendChild(c1a);
+    c1b=document.createElement("div");
+    c1b.className="w3-col m5 w3-section";
+    c1b.innerHTML="<input class=\"w3-input\" style=\"width:90%\" type=\"text\" "+
+            " pattern=\"^([0-9]|[0-1][0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$\" "+
+            " id=\"sekLi"+i+"\" name=\"sekLi"+i+"\" value=\"\" placeholder=\"Sekcja "+v+"\" required/>";
+    //k.appendChild(c1b);
+    c1c=document.createElement("div");
+    c1c.className="w3-col m2 w3-section ";
+    c1c.innerHTML=" <button type='button' class='btn button5' style='width:80%;' onclick='setWifi()'> "+
+            "<i style='color:green' class='fa fa-edit w3-xlarge' aria-hidden='true' ></i></button>";
+    //k.appendChild(c1c);
+    w.appendChild(c1a);
+    w.appendChild(c1b);
+    w.appendChild(c1c);
+    //w.appendChild(k);
+    f.appendChild(w);
+}

80d6bcbe527cf3177c100d76a097ee9630466eb5
kzg <krzysztof.zagawa@external.t-mobile.pl>
2018-05-16 16:59:15 +0200

    jsonowanie formow progr

--- data/js/wsProg.js
+++ data/js/wsProg.js
@@ -172,7 +172,7 @@ function addSekList(v,n)
     //if(true)
     //w.classList.add("w3-light-grey");
     //else w.classList.add("w3-dark-grey");
-    w.id="Sek"+i;
+    w.id="SekLbl"+v;
 	//let k= document.createElement("div");
     //k.className="w3-third w3-section";
     c1a=document.createElement("div");
@@ -186,7 +186,7 @@ function addSekList(v,n)
     //k.appendChild(c1b);
     c1c=document.createElement("div");
     c1c.className="w3-col m1 w3-section ";
-    c1c.innerHTML=" <button type='button' class='btn button5' style='width:80%;' onclick='setWifi()'> "+
+    c1c.innerHTML=" <button type='button' class='btn button5' style='width:80%;' onclick=\"KonfForm('SekLbl"+i+"')\"> "+
             "<i style='color:green' class='fa fa-edit w3-xlarge' aria-hidden='true' ></i></button>";
     //k.appendChild(c1c);
     w.appendChild(c1a);
@@ -199,22 +199,141 @@ function addSekList(v,n)
 function KonfForm(id)
 {
 
-    var x = $("WifiForm").serializeArray();
-    $.each(x, function(i, field){
-        $("#results").append(field.name + ":" + field.value + " ");
-    });
+    let x = document.getElementById(id).elements;//.serializeArray();
+    let v =true;
+    for(let i=0;i<x.length;i++)
+    {
+    
+        if(!x[i].validity.valid)  v=false;
+    }
+    if(!v)
+    {
+        console.log("blad walidacji "+id);
+        //return;
+    }
+    
+    const data = formToJSON(x);
 
-    let f=document.getElementById(id).elements;
-    console.log("KonfForm "+f);
-    var unindexed_array = f.serializeArray();
-    var indexed_array = {};
 
-    $.map(unindexed_array, function(n, i){
-        indexed_array[n['name']] = n['value'];
-    });
-
-
-    console.log(unindexed_array);
-        console.log(indexed_array);
-    return indexed_array;
+    
+ //   console.log("json="+JSON.stringify(data));
+    let str={"topic":id,"msg":data};
+    console.log(JSON.stringify(str));
+   
 }
+
+//// IA JSON 
+// setNTP  {topic:setNTP; msg:{host:adr; offset:h}} ustawia parametry zegara NTP
+// setTime {topic:setTime; msg:{dzien:x;czas:str}} ustawia parametry zegara
+// setWifi {topic:setWifi; msg:{typ:a/d/p; ssid:s;pass:p}} add/del/publish ssid passwd
+// setMQTT {topic:setMqtt; msg:{host:adr; p:port; l:login; h:passwd}} ustawienia mqtt
+// setSek  {topic:setSek;  msg:{id:i; n:nazwa}}
+///
+
+
+/**
+ * Checks that an element has a non-empty `name` and `value` property.
+ * @param  {Element} element  the element to check
+ * @return {Bool}             true if the element is an input, false if not
+ */
+const isValidElement = element => {
+    return element.name && element.value;
+  };
+  
+  /**
+   * Checks if an element’s value can be saved (e.g. not an unselected checkbox).
+   * @param  {Element} element  the element to check
+   * @return {Boolean}          true if the value should be added, false if not
+   */
+  const isValidValue = element => {
+    return (!['checkbox', 'radio'].includes(element.type) || element.checked);
+  };
+  
+  /**
+   * Checks if an input is a checkbox, because checkboxes allow multiple values.
+   * @param  {Element} element  the element to check
+   * @return {Boolean}          true if the element is a checkbox, false if not
+   */
+  const isCheckbox = element => element.type === 'checkbox';
+  const isBtn = element => element.type === 'button';
+  
+  /**
+   * Checks if an input is a `select` with the `multiple` attribute.
+   * @param  {Element} element  the element to check
+   * @return {Boolean}          true if the element is a multiselect, false if not
+   */
+  const isMultiSelect = element => element.options && element.multiple;
+  
+  /**
+   * Retrieves the selected options from a multi-select as an array.
+   * @param  {HTMLOptionsCollection} options  the options for the select
+   * @return {Array}                          an array of selected option values
+   */
+  const getSelectValues = options => [].reduce.call(options, (values, option) => {
+    return option.selected ? values.concat(option.value) : values;
+  }, []);
+  
+  /**
+   * A more verbose implementation of `formToJSON()` to explain how it works.
+   *
+   * NOTE: This function is unused, and is only here for the purpose of explaining how
+   * reducing form elements works.
+   *
+   * @param  {HTMLFormControlsCollection} elements  the form elements
+   * @return {Object}                               form data as an object literal
+   */
+  const formToJSON_deconstructed = elements => {
+    
+    // This is the function that is called on each element of the array.
+    const reducerFunction = (data, element) => {
+      
+      // Add the current field to the object.
+      data[element.name] = element.value;
+      
+      // For the demo only: show each step in the reducer’s progress.
+      console.log(JSON.stringify(data));
+  
+      return data;
+    };
+    
+    // This is used as the initial value of `data` in `reducerFunction()`.
+    const reducerInitialValue = {};
+    
+    // To help visualize what happens, log the inital value, which we know is `{}`.
+    console.log('Initial `data` value:', JSON.stringify(reducerInitialValue));
+    
+    // Now we reduce by `call`-ing `Array.prototype.reduce()` on `elements`.
+    const formData = [].reduce.call(elements, reducerFunction, reducerInitialValue);
+    
+    // The result is then returned for use elsewhere.
+    return formData;
+  };
+  
+  /**
+   * Retrieves input data from a form and returns it as a JSON object.
+   * @param  {HTMLFormControlsCollection} elements  the form elements
+   * @return {Object}                               form data as an object literal
+   */
+  const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+  
+    // Make sure the element has the required properties and should be added.
+    if (isValidElement(element) && isValidValue(element)) {
+  
+      /*
+       * Some fields allow for more than one value, so we need to check if this
+       * is one of those fields and, if so, store the values as an array.
+       */
+      if(isBtn(element))return;
+      if (isCheckbox(element)) {
+        data[element.name] = (data[element.name] || []).concat(element.value);
+      } else if (isMultiSelect(element)) {
+        data[element.name] = getSelectValues(element);
+      } else {
+        data[element.name] = element.value;
+      }
+    }
+  
+    return data;
+  }, {});
+  
+  
